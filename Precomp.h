354d0e694f650156e4f6bf4ba4ae8c9b573395b5
ArseniMokhau <99400189+ArseniMokhau@users.noreply.github.com>
2022-02-10 14:20:01 +0300

    Add files via upload

---
+++ Precomp.h
@@ -0,0 +1,15 @@
+
+//SFML//
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+
+//Other//
+#include <iostream>
+#include <ctime>
+#include <time.h>
+#include <cstdlib>
+
+
