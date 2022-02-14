0507f5f5a8c89296877d7b7004cdeb79302a09d1
ArseniMokhau <99400189+ArseniMokhau@users.noreply.github.com>
2022-02-14 23:52:44 +0300

    Add files via upload

---
+++ PlatformerSFML/map/lvl3/background32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="background4" tilewidth="32" tileheight="32" tilecount="156" columns="13">
+ <image source="background4.png" width="426" height="384"/>
+</tileset>
