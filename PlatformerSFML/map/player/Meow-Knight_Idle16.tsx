05db1e59821ac4fb17bbac0baa899f1caf2a9aff
ArseniMokhau <99400189+ArseniMokhau@users.noreply.github.com>
2022-02-14 23:54:52 +0300

    Add files via upload

---
+++ PlatformerSFML/map/player/Meow-Knight_Idle16.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Meow-Knight_Idle16" tilewidth="16" tileheight="32" tilecount="4" columns="1">
+ <image source="Meow-Knight_Idle.png" width="16" height="146"/>
+</tileset>
