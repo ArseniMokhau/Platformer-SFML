451463118a92a281a5afd7d503109db80eda0f3e
ArseniMokhau <99400189+ArseniMokhau@users.noreply.github.com>
2022-02-14 23:55:28 +0300

    Add files via upload

---
+++ PlatformerSFML/map/slime/Slime_Spiked_Full.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Slime_Spiked_Full" tilewidth="20" tileheight="16" tilecount="480" columns="19">
+ <grid orientation="orthogonal" width="16" height="16"/>
+ <image source="Slime_Spiked_Full.png" width="384" height="320"/>
+</tileset>
