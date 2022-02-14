1ddc8a3f2e3415d1b5dbc6f0f025101f169fe13f
ArseniMokhau <99400189+ArseniMokhau@users.noreply.github.com>
2022-02-14 23:48:59 +0300

    Add files via upload

---
+++ PlatformerSFML/map/cave/main_lev_buildA.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="main_lev_buildA" tilewidth="32" tileheight="32" tilecount="1764" columns="42">
+ <image source="main_lev_buildA.png" width="1344" height="1344"/>
+</tileset>
