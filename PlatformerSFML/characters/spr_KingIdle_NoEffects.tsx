d57e62eccdcd08a63b06d9efc0a8a7214800be53
ArseniMokhau <99400189+ArseniMokhau@users.noreply.github.com>
2022-02-14 23:50:12 +0300

    Add files via upload

---
+++ PlatformerSFML/characters/spr_KingIdle_NoEffects.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="spr_KingIdle_NoEffects" tilewidth="32" tileheight="32" tilecount="288" columns="72">
+ <image source="spr_KingIdle_NoEffects.png" width="2304" height="128"/>
+</tileset>
